4176a64506bb9e2cbe854d92c72b468d24e22c49
Natalia Horbunova <nata.horbunova@gmail.com>
2023-04-10 02:49:19 +0300

    refactored, fixed geolcocation, modified forecast data

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,8 @@
 class WeatherStorage {
-    #baseUrl = 'http://api.openweathermap.org/'
-    #appid = '31fdbea09ec46cb0d196ca9d28ad0701'
-    currenCity;
+    #baseUrl = 'http://api.openweathermap.org/';
+    #appid = '31fdbea09ec46cb0d196ca9d28ad0701';
 
     async requestData(url) {
-        // const url = `${this.#baseUrl}data/2.5/forecast?id=703448&appid=${this.#appid}`
         const response = await fetch(url);
 
         if (response.ok) {
@@ -15,20 +13,15 @@ class WeatherStorage {
     }
 
     getFromLocalStorage() {
-        let city = localStorage.getItem('city');
-        if (city) {
-            this.currenCity = city;
-        }
-        return city;
+        return localStorage.getItem('city');
     }
 
     setToLocalStorage(city) {
         localStorage.setItem('city', city);
-        this.currenCity = city;
     }
 
-    async getCityLocation() {
-        const url = `${this.#baseUrl}/geo/1.0/direct?q=${this.currenCity},&appid=${this.#appid}`
+    async getCityLocation(city) {
+        const url = `${this.#baseUrl}/geo/1.0/direct?q=${city},&appid=${this.#appid}`
         return await this.requestData(url);
     }
 
@@ -37,90 +30,192 @@ class WeatherStorage {
         return await this.requestData(url);
     }
 
-   getCurrentLocationData() {
-      //   let locationData;
-      //
-      // navigator.geolocation.getCurrentPosition((pos) => {
-      //           locationData = [{
-      //           lat: pos.coords.latitude,
-      //           lon: pos.coords.longitude
-      //       }]
-      //   })
-      //
-      //   console.log(locationData);
-      //   return locationData;
+    async getForecastWeatherData(lat, lon) {
+        const url = `${this.#baseUrl}data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${this.#appid}`;
+        return await this.requestData(url);
     }
 
+    async getCurrentLocationData() {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition((pos) => {
+                    resolve([{
+                        lat: pos.coords.latitude,
+                        lon: pos.coords.longitude
+                    }])
+                },
+                function (error) {
+                    reject(error)
+                })
+        })
+    }
 }
 
 class WeatherModel {
     cityData = '';
+    respForecastData = '';
+    modifiedForecastData = [];
 
     constructor(storage) {
         this.weatherStorage = storage;
     }
 
-    async getData() {
-        localStorage.setItem('city', 'Odessa'); // TEST!!!!!!!!
+    async initRequest() {
+        const city = this.weatherStorage.getFromLocalStorage();
 
-        let cityLocationData;
+        if (!city) {
+            try {
+                const locationData = await this.weatherStorage.getCurrentLocationData();
+                if (locationData) await this.getDataByLocation(locationData);
+                this.weatherStorage.setToLocalStorage(this.cityData.name)
 
-        if (!this.weatherStorage.currenCity) {
-            const city = this.weatherStorage.getFromLocalStorage();
-            if (!city) {
-                cityLocationData = this.weatherStorage.getCurrentLocationData();
-            } else {
-                cityLocationData = await this.weatherStorage.getCityLocation();
+            } catch (error) {
+                console.error(error);
             }
         } else {
-            cityLocationData = await this.weatherStorage.getCityLocation();
+            await this.getDataByCityName(city);
         }
+    }
 
+    async getDataByCityName(city) {
+        const locationData = await this.weatherStorage.getCityLocation(city);
+        if (locationData) await this.getDataByLocation(locationData);
+    }
+
+    async getDataByLocation(cityLocationData) {
         const data = await this.weatherStorage.getCurrentWeatherData(cityLocationData[0].lat, cityLocationData[0].lon);
         if (data) this.cityData = data;
-        console.log('getDataModel');
+
+        const forecast = await this.weatherStorage.getForecastWeatherData(cityLocationData[0].lat, cityLocationData[0].lon);
+        if (forecast) this.respForecastData = forecast;
+        this.modifyForecastData();
+
 
         // await this.weatherStorage.requestData()
         //     .then(resp => this.data = resp)
         //     .catch(error => alert(error.message));
     }
+
+    modifyForecastData() {
+        const forecastSorted = this.respForecastData.list.reduce(function (acc, el) {
+            let date = el.dt_txt.split(' ')[0];
+
+            if (!acc[date]) acc[date] = {
+                temp_max: [],
+                temp_min: [],
+                pressure: [],
+                windSpeed: [],
+                weather: []
+            };
+
+            acc[date].temp_max.push(el.main.temp_max);
+            acc[date].temp_min.push(el.main.temp_min);
+            acc[date].pressure.push(el.main.pressure);
+            acc[date].windSpeed.push(el.wind.speed);
+            acc[date].weather.push(el.weather[0].main);
+
+            return acc;
+        }, {});
+
+        for (let date in forecastSorted) {
+            const tempMax = this.findMaxValue(forecastSorted[date].temp_max);
+            const tempMin = this.findMinValue(forecastSorted[date].temp_min);
+            const pressure = this.findAvgValue(forecastSorted[date].pressure, 0);
+            const windSpeed = this.findAvgValue(forecastSorted[date].windSpeed, 2);
+            const weather = this.findMostRepeatedValue(forecastSorted[date].weather);
+
+            const forecastForDay = {
+                weather: `[{main: ${weather}}]`,
+                main: {
+                    temp_max: tempMax,
+                    temp_min: tempMin,
+                    pressure
+                },
+                wind: {speed: windSpeed},
+                sys: {
+                    sunrise: this.respForecastData.city.sunrise,
+                    sunset: this.respForecastData.city.sunset
+                }
+            };
+
+            this.modifiedForecastData.push(forecastForDay);
+        }
+    }
+
+    findMinValue(arr) {
+        let minValue = arr[0];
+
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] < minValue) {
+                minValue = arr[i];
+            }
+        }
+        return minValue;
+    }
+
+    findMaxValue(arr) {
+        let maxVal = arr[0];
+
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+
+    findAvgValue(arr, decimalPlaces) {
+        const sum = arr.reduce((acc, val) => acc + val, 0);
+        const avg = sum / arr.length;
+        return +avg.toFixed(decimalPlaces);
+    }
+
+   findMostRepeatedValue(arr) {
+        const counts = {};
+        let mostRepeatedVal = arr[0];
+
+        arr.forEach((val) => {
+            counts[val] = (counts[val] || 0) + 1;
+            if (counts[val] > counts[mostRepeatedVal]) {
+                mostRepeatedVal = val; // обновляем наиболее повторяющееся значение
+            }
+        });
+
+        return mostRepeatedVal;
+    }
 }
 
 class WeatherView {
-    cityNameInput = document.querySelector('.city-input');
+    inputCityName = document.querySelector('.city-input');
 
     constructor(model) {
         this.weatherModel = model;
-    }
-
-    async getData() {
-        await this.weatherModel.getData();
-        console.log('getDataView')
-
+        this.weatherStorage = model.weatherStorage;
     }
 
     getCityOutput() {
-        this.cityNameInput.addEventListener('keydown', async (e) => {
-            if (e.key === "Enter" && this.cityNameInput.value) {
-                this.weatherModel.weatherStorage.setToLocalStorage(this.cityNameInput.value);
-                // localStorage.setItem('city', this.cityNameInput.value);
-                await this.weatherModel.getData();
+        this.inputCityName.addEventListener('keydown', async (e) => {
+            if (e.key === "Enter" && this.inputCityName.value) {
+                await this.weatherModel.getDataByCityName(this.inputCityName.value);
+                this.weatherStorage.setToLocalStorage(this.inputCityName.value);
                 this.render();
             }
         })
-        console.log('getCityOutput');
-
     }
 
     render() {
-        // console.log('render');
+        if (!this.weatherModel.cityData) {
+            // !!!!!! Show Kiev data
+        }
+        // !!! shw current city data
         console.log(this.weatherModel.cityData);
+        console.log(this.weatherModel.respForecastData);
+        console.log(this.weatherModel.modifiedForecastData);
     }
 
     async init() {
-        await this.getData();
-        this.getCityOutput();
+        await this.weatherModel.initRequest();
         this.render();
+        this.getCityOutput();
     }
 }
 
